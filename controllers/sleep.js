a16adfa47bd77c6ef2ffe518b5f2fec3b101ea86
brianplynn <brnlynn889@gmail.com>
2019-02-22 22:24:03 -0800

    added sleep routes

--- controllers/sleep.js
+++ controllers/sleep.js
@@ -1,6 +1,6 @@
 const getSleepData = (req, res, db) =>  {
 	const { id } = req.params;
-	db.select('weekday', 'hours', 'quality')
+	db.select('sleep_date', 'hours', 'quality')
 		.from('sleep')
 		.where('user_id', '=', id)
 		.then(data => res.json(data))
@@ -8,12 +8,34 @@ const getSleepData = (req, res, db) =>  {
 }
 
 const addSleep = (req, res, db) =>  {
-	const { data, weekday } = req.body;
-	res.json([data, weekday]);
+	const { data, user } = req.body;
+	const formattedData = Object.assign({}, { user_id: user, hours: Number(data.hours)+Number(data.minutes)/60, quality: data.quality, sleep_date: data.date });
+	db('sleep')
+    .insert(formattedData)
+    .returning('user_id')
+    .then(id => res.json(id));
 }
 
 const editSleep = (req, res, db) => {
-	res.json(req.body);
+	const { data, user } = req.body;
+	const formattedData = data.map( item => {
+		return Object.assign({}, { user_id: user, hours: Number(item.hours)+Number(item.minutes)/60, quality: item.quality, sleep_date: item.date });
+	});
+	db.transaction( trx => {
+		trx('sleep').where({
+			user_id: user
+		})
+		.del()
+		.then(nums => {
+			return trx('sleep')
+				   .insert(formattedData)
+				   .returning('sleep_date')
+				   .then(id => res.json(id));
+		})
+		.then(trx.commit)
+		.catch(trx.rollback)
+	})
+	.catch(err => res.status(400).json('Unable to submit workout'));
 }
 
 module.exports = {
